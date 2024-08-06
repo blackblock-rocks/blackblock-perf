697cdd0c09a6487345d7242aee546da9578b2bed
Jelle De Loecker <jelle@elevenways.be>
2024-08-06 17:01:41 +0200

    💪 Delay teleport until after all worlds have ticked

---
+++ src/main/java/rocks/blackblock/perf/mixin/EntityMixin.java
@@ -0,0 +1,38 @@
+package rocks.blackblock.perf.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.TeleportTarget;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rocks.blackblock.perf.thread.BlackblockThreads;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+
+    @Shadow public abstract @Nullable Entity teleportTo(TeleportTarget teleportTarget);
+
+    /**
+     * Force teleportations to happen on the main server thread,
+     * after all the worlds have ticked concurrently.
+     * This way we avoid any CME or deadlocks without too much hassle.
+     */
+    @Inject(method = "teleportTo", at = @At("HEAD"), cancellable = true)
+    public void moveToWorld(TeleportTarget teleportTarget, CallbackInfoReturnable<Entity> cir) {
+
+        if (!BlackblockThreads.THREADS_ENABLED) {
+            return;
+        }
+
+        if (BlackblockThreads.ownsThread(Thread.currentThread())) {
+            teleportTarget.world().getServer().execute(() -> {
+                this.teleportTo(teleportTarget);
+            });
+
+            cir.setReturnValue(null);
+        }
+    }
+}
