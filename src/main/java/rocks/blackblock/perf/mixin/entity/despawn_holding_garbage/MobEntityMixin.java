e8e4cbf41ca12e4772975a24ed0c7565953bf1e4
Jelle De Loecker <jelle@elevenways.be>
2024-08-08 17:42:42 +0200

    ✨ Don't let mobs become persistent when they pick up garbage

---
+++ src/main/java/rocks/blackblock/perf/mixin/entity/despawn_holding_garbage/MobEntityMixin.java
@@ -0,0 +1,105 @@
+package rocks.blackblock.perf.mixin.entity.despawn_holding_garbage;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import rocks.blackblock.bib.util.BibEntity;
+import rocks.blackblock.bib.util.BibItem;
+
+/**
+ * Don't make mobs persistent just because they picked up garbage
+ *
+ * @author   Jelle De Loecker <jelle@elevenways.be>
+ * @since    0.1.0
+ */
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+
+    @Shadow private boolean persistent;
+
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    /**
+     * See if the mob is picking up garbage.
+     * If it is, cancel setting the persistence.
+     * @since    0.1.0
+     */
+    @Inject(
+        method = "equipLootStack",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/entity/mob/MobEntity;persistent:Z",
+            opcode = Opcodes.PUTFIELD,
+            shift = At.Shift.BEFORE
+        ),
+        cancellable = true
+    )
+    private void bb$modifyPersistentValue(EquipmentSlot slot, ItemStack stack, CallbackInfo ci) {
+
+        boolean is_garbage = BibItem.isGarbage(stack);
+
+        // Allow the mob to be turned persistent
+        // when it's picking up something that's not garbage
+        if (!is_garbage) {
+            return;
+        }
+
+        boolean is_persistent = this.persistent;
+
+        // If it's already persistent, see if it's holding something that's not garbage
+        if (is_persistent) {
+
+            // Don't prevent persistency when the mob has a custom name
+            if (this.hasCustomName()) {
+                return;
+            }
+
+            // If it's not only holding garbage, don't prevent persistency
+            if (!BibEntity.isOnlyHoldingGarbage((LivingEntity) (Object) this)) {
+                return;
+            }
+        }
+
+        ci.cancel();
+    }
+
+    /**
+     * If a mob is loaded, and it's persistent,
+     * make sure it's holding something that's not garbage.
+     * @since    0.1.0
+     */
+    @Inject(
+        method = "readCustomDataFromNbt",
+        at = @At(
+            value = "RETURN"
+        )
+    )
+    private void bb$afterReadNbt(CallbackInfo ci) {
+
+        // Nothing needs to be done if the mob is not persistent
+        if (!this.persistent) {
+            return;
+        }
+
+        // Keep it persistent if it has a custom name
+        if (this.hasCustomName()) {
+            return;
+        }
+
+        // Turn off persistency if it's holding only garbage
+        if (BibEntity.isOnlyHoldingGarbage((LivingEntity) (Object) this)) {
+            this.persistent = false;
+        }
+    }
+}
