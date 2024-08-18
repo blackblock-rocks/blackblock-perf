6262b090672cc60112f03f7e992ac8e2075c0838
Jelle De Loecker <jelle@elevenways.be>
2024-08-18 11:59:17 +0200

    ♻️ Don't load composter alloc improvements when lithium is loaded

--- src/main/java/rocks/blackblock/perf/mixin/reduce_alloc/ComposterFullInventoryMixinForReducedAlloc.java
+++ src/main/java/rocks/blackblock/perf/mixin/reduce_alloc/ComposterFullInventoryMixinForReducedAlloc.java
@@ -1,11 +1,13 @@
 package rocks.blackblock.perf.mixin.reduce_alloc;
 
+import com.moulberry.mixinconstraints.annotations.IfModAbsent;
 import net.minecraft.block.ComposterBlock;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import rocks.blackblock.perf.dedup.Constants;
 
+@IfModAbsent("lithium")
 @Mixin(ComposterBlock.FullComposterInventory.class)
 public class ComposterFullInventoryMixinForReducedAlloc {
     /**
