9cc30cc1397c58a4407818f21dcd87d632c82836
Jelle De Loecker <jelle@elevenways.be>
2025-01-19 14:24:12 +0100

    🐛 Make sure the `createTeleportTarget` call also happens on the server thread

--- src/main/java/rocks/blackblock/perf/mixin/threading/EntityMixinForTeleportFix.java
+++ src/main/java/rocks/blackblock/perf/mixin/threading/EntityMixinForTeleportFix.java
@@ -1,20 +1,43 @@
 package rocks.blackblock.perf.mixin.threading;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.PortalManager;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import rocks.blackblock.perf.thread.DynamicThreads;
 
 @Mixin(Entity.class)
 public abstract class EntityMixinForTeleportFix {
 
-    @Shadow public abstract @Nullable Entity teleportTo(TeleportTarget teleportTarget);
+    @Shadow
+    public abstract @Nullable Entity teleportTo(TeleportTarget teleportTarget);
+
+    @Shadow
+    public abstract World getWorld();
+
+    @Shadow
+    protected abstract void tickPortalCooldown();
+
+    @Shadow
+    @Nullable public PortalManager portalManager;
+
+    @Shadow
+    public abstract boolean canUsePortals(boolean allowVehicles);
+
+    @Shadow
+    public abstract void resetPortalCooldown();
+
+    @Shadow
+    public abstract boolean canTeleportBetween(World from, World to);
 
     /**
      * Force teleportations to happen on the main server thread,
@@ -42,5 +65,78 @@ public abstract class EntityMixinForTeleportFix {
         });
 
         cir.setReturnValue(null);
+    }
+
+    /**
+     * The same way we put the actual teleport logic on the server thread,
+     * we also have to put the portal teleportation preparation logic on
+     * the server thread.
+     */
+    @Inject(method = "tickPortalTeleportation", at = @At("HEAD"), cancellable = true)
+    private void onTickPortalTeleportation(CallbackInfo ci) {
+
+        // Keep all the original logic if threading is disabled
+        if (!DynamicThreads.THREADS_ENABLED) {
+            return;
+        }
+
+        // Prevent the original method from running
+        ci.cancel();
+
+        var world = this.getWorld();
+
+        if (!(world instanceof ServerWorld serverWorld)) {
+            return;
+        }
+
+        this.tickPortalCooldown();
+
+        if (this.portalManager == null) {
+            return;
+        }
+
+        Entity self = (Entity) (Object) this;
+        boolean canUsePortals = this.canUsePortals(false);
+        boolean tickedPortal = this.portalManager.tick(serverWorld, self, canUsePortals);
+
+        if (!tickedPortal) {
+
+            if (this.portalManager.hasExpired()) {
+                this.portalManager = null;
+            }
+
+            return;
+        }
+
+        this.resetPortalCooldown();
+
+        world.getServer().execute(this::bb$continuePortalTeleportation);
+    }
+
+    /**
+     * Actually continue the portal teleportation logic
+     * (Should run on the server thread)
+     */
+    @Unique
+    private void bb$continuePortalTeleportation() {
+
+        var world = this.getWorld();
+
+        if (!(world instanceof ServerWorld serverWorld)) {
+            return;
+        }
+
+        TeleportTarget teleportTarget = this.portalManager.createTeleportTarget(serverWorld, (Entity) (Object) this);
+
+        if (teleportTarget == null) {
+            return;
+        }
+
+        ServerWorld targetWorld = teleportTarget.world();
+
+        if (serverWorld.getServer().isWorldAllowed(targetWorld)
+                && (targetWorld.getRegistryKey() == serverWorld.getRegistryKey() || this.canTeleportBetween(serverWorld, targetWorld))) {
+            this.teleportTo(teleportTarget);
+        }
     }
 }
